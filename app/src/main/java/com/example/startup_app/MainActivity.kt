0125b52993225144e4c0830a1efc56540b99bd65
deepak <deepaksingh251000@gmail.com>
2021-04-23 19:49:45 +0530

    this is a new commit

--- app/src/main/java/com/example/startup_app/MainActivity.kt
+++ app/src/main/java/com/example/startup_app/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.startup_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import androidx.annotation.NonNull
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -15,7 +19,7 @@ import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var bottom_nav_bar : BottomNavigationView
 
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +28,22 @@ class MainActivity : AppCompatActivity() {
 
         auth = Firebase.auth
 
+        bottom_nav_bar = findViewById(R.id.bottom_nav_bar)
+        bottom_nav_bar.setOnNavigationItemSelectedListener {
+                when(it.itemId){
+                    R.id.profile -> supportFragmentManager.beginTransaction().apply { replace(R.id.frame , profile()).commit() }
+
+                    R.id.camera ->supportFragmentManager.beginTransaction().apply { replace(R.id.frame ,camera()).commit() }
+                    R.id.home ->supportFragmentManager.beginTransaction().apply { replace(R.id.frame ,home()).commit() }
+
+                }
+
+                 true
+            }
+
+
     }
+
 
     override fun onStart() {
         super.onStart()
