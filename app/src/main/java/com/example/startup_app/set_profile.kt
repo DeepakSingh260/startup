82b1c8d434c002a10e3dd1adbe47b2805223b880
deepak <deepaksingh251000@gmail.com>
2021-04-26 11:49:33 +0530

    message

--- app/src/main/java/com/example/startup_app/set_profile.kt
+++ app/src/main/java/com/example/startup_app/set_profile.kt
@@ -1,16 +1,30 @@
-package com.example.startup_app
+ package com.example.startup_app
 
+import android.content.ContentResolver
+import android.content.ContentUris
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContentResolverCompat.query
 
-class set_profile : AppCompatActivity() {
+ data class Image(val uri: Uri ,
+    val  name:String ,
+    val size:Int
+)
+
+ private val imageList = mutableListOf<Image>()
+
+
+ class set_profile : AppCompatActivity() {
 
     private lateinit var profile_img : ImageView
     private lateinit var change_img : Button
@@ -19,34 +33,53 @@ class set_profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_set_profile)
+
+
+        val projection = arrayOf(MediaStore.Images.Media.ORIGINAL_DOCUMENT_ID,MediaStore.Images.Media.DISPLAY_NAME , MediaStore.Images.Media.SIZE,)
+        val selection = "${MediaStore.Images.Media.SIZE}<=?"
+        val selectionArgs = arrayOf("2")
+        val sortOrder = "${MediaStore.Images.Media.DISPLAY_NAME} ASC"
+
+        applicationContext.contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                , projection
+                , selection
+                ,selectionArgs
+                , sortOrder)?.use { cursor->
+
+                    val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.ORIGINAL_DOCUMENT_ID)
+                    val nameColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
+                    val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.SIZE)
+
+                    while (cursor.moveToNext()){
+
+                        val id = cursor.getLong(idColumn)
+                        val name = cursor.getString(nameColumn)
+                        val size  = cursor.getInt(sizeColumn)
+
+                        val contentUri:Uri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,id)
+                        imageList += Image(contentUri , name , size)
+
+
+
+                    }
+
+        }
+
+
+
         profile_img = findViewById(R.id.select_img)
         change_img = findViewById(R.id.select_photo)
 
-        change_img.setOnClickListener {
-            (object : View.OnClickListener {
-
-              override fun onClick(v:View){
-
-                 openGallery()
+        change_img.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                Toast.makeText(this@set_profile , "open gallery" , Toast.LENGTH_SHORT).show()
             }
-        } )}
-
-
+        })
     }
 
-    private fun openGallery(){
-        startActivityForResult(Intent(Intent.ACTION_PICK , MediaStore.Images.Media.INTERNAL_CONTENT_URI),PICK_IMAGE)
 
+
+    companion object{
+        private const val TAG = "set profile"
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
-            if (data != null) {
-                imageUri = data.data!!
-            }
-            profile_img.setImageURI(imageUri)
-        }
-    }
-
 }
